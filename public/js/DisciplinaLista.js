8c5e01f5bdfab8e9404e3304c7a6712a10d8c260
Wadyweb73 <wadymunguambe73@gmail.com>
2024-09-14 11:30:23 +0200

    feat: add ListarDisciplina to frontend

--- public/js/DisciplinaLista.js
+++ public/js/DisciplinaLista.js
@@ -1,5 +1,5 @@
-export async function listarCursos() {
-    const response = await fetch('/sige_tutorias/cursos', {
+export async function listarDisciplinas() {
+    const response = await fetch('/sige_tutorias/disciplinas', {
         method: 'GET'
     });
  
@@ -9,3 +9,31 @@ export async function listarCursos() {
 
     return await response.json();
 }
+
+async function updatePageContent() {
+    const response = await listarDisciplinas();
+    var content = "";
+
+    console.log(response);
+    
+    response.forEach((disciplina) => {
+        const html = `
+            <tr>
+                <th>${disciplina.id_disciplina}</th>
+                <th>${disciplina.nome_disciplina}</th>
+                <th>${disciplina.id_curso}</th>
+            </tr>
+        `;
+
+        content += html;
+    })
+
+    document.querySelector('.js-table-body')
+        .innerHTML = content;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    updatePageContent();
+    
+});
