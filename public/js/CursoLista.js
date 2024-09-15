0e169d5030d928bc94552723ef78de7f87a40bb9
Wadyweb73 <wadymunguambe73@gmail.com>
2024-09-15 03:11:48 +0200

    feat: imprime nomes das faculdades em vez de ID em CursoLista

--- public/js/CursoLista.js
+++ public/js/CursoLista.js
@@ -1,3 +1,5 @@
+import { getFaculdadeById } from "./FaculdadeLista.js";
+
 export async function listarCursos() {
     const response = await fetch('/sige_tutorias/cursos', {
         method: 'GET'
@@ -14,17 +16,19 @@ async function updatePageContent() {
     const response    = await listarCursos();
     var table_content = "";
     
-    response.forEach((curso) => {
+    for (const faculdade of response) {
+        const faculdade_res = await getFaculdadeById(faculdade.id_faculdade);
+        
         const html = `
-            <tr>
-            <td>${curso.id_curso}</td>
-            <td>${curso.nome_curso}</td>    
-                <td>${curso.id_faculdade}</td>
+                <tr>
+                <td>${curso.id_curso}</td>
+                <td>${curso.nome_curso}</td>    
+                <td>${faculdade_res.nome_facul}</td>
             </tr>
         `;
 
         table_content += html;
-    });
+    };
 
     document.querySelector('.js-table-body')
         .innerHTML = `${table_content}`;
